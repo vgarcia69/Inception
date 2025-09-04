fc9538bf966b68c33e96af7a4b2ce2c01fbdc9ca
root <vincent.garcia6942@gmail.com>
2025-09-04 14:07:50 +0200

    avance minime

---
+++ srcs/requirements/mariadb/script.sh
@@ -0,0 +1,8 @@
+# recuprer les lignes du .env pour les export dans le container
+# export $(cat srcs/.env) 
+# ############## ou #######
+# set -a
+# source srcs/.env    
+
+
+mysql -u root -p $MARIADB_ADMIN_PASSWD
--- srcs/requirements/wordpress/wp-config.php
+++
@@ -1,4 +0,0 @@
-<!-- define('DB_HOST', 'mariadb:3306');
-define('DB_NAME', 'wordpress');
-define('DB_USER', 'wordpress');
-define('DB_PASSWORD', 'password');  -->

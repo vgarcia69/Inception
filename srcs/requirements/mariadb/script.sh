a7a49a0dbeea26d89a07d409369883bb6b067824
vince <vince@vince.vince>
2025-09-09 14:30:28 +0200

    l

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# recuprer les lignes du .env pour les export dans le container
-# export $(cat srcs/.env) 
-# ############## ou #######
-# set -a
-# source /etc/mariadb/.env    
-# set +a
+set -e
 
-cd /etc/mariadb/
+# #log in the admin user
+# exec mysql -u root -p $MARIADB_ADMIN_PASSWD
 
-./init.sql
 
-echo "mdp: $MARIADB_ADMIN_PASSWD." && sync
 
-mysql -u root -p $MARIADB_ADMIN_PASSWD
+envsubst < /init.sql > /tmp/init_tmp.sql
+
+chmod +x /tmp/init_tmp.sql
+
+
+
+mysqld --user=mysql --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock
+
+
+mysql -u root -p ${MARIADB_ROOT_PASSWD} < /tmp/init_tmp.sql
